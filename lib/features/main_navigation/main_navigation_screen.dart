42f840878bf96776f3b33c3448350ceb4ea6d007
ashsho412 <ashsh0412@gmail.com>
2023-11-07 00:25:25 +0900

    video post & video errors

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -4,7 +4,7 @@ import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_button.dart';
-import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
+import 'package:tiktok_clone/features/videos/widgets/video_timeline_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
