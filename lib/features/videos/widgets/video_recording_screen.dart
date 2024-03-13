0ce803e97db2cbe5448c6576a252970f89c089ce
ashsho412 <ashsh0412@gmail.com>
2024-03-13 15:47:21 +0900

    video setting

--- lib/features/videos/widgets/video_recording_screen.dart
+++ lib/features/videos/widgets/video_recording_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_preview_screen.dart';
 
 class VideoRecordingScrenn extends StatefulWidget {
   const VideoRecordingScrenn({super.key});
@@ -11,10 +12,32 @@ class VideoRecordingScrenn extends StatefulWidget {
   State<VideoRecordingScrenn> createState() => _VideoRecordingScrennState();
 }
 
-class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
+class _VideoRecordingScrennState extends State<VideoRecordingScrenn>
+    with TickerProviderStateMixin {
   bool _hasPermission = false;
 
   bool _isSelfieMode = false;
+
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(
+      microseconds: 300,
+    ),
+  );
+
+  late final Animation<double> _buttonAnimation = Tween(
+    begin: 1.0,
+    end: 1.3,
+  ).animate(_buttonAnimationController);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 8),
+    lowerBound: 0,
+    upperBound: 1,
+  );
 
   late FlashMode _flashMode;
 
@@ -32,6 +55,8 @@ class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
     );
 
     await _cameraController.initialize();
+
+    await _cameraController.prepareForVideoRecording();
 
     _flashMode = _cameraController.value.flashMode;
   }
@@ -57,6 +82,9 @@ class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
   void initState() {
     super.initState();
     initPermissions();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
   }
 
   Future<void> _toggleSelfieMode() async {
@@ -69,6 +97,37 @@ class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
     await _cameraController.setFlashMode(newFlashMode);
     _flashMode = newFlashMode;
     setState(() {});
+  }
+
+  Future<void> _startRecording(TapDownDetails _) async {
+    if (_cameraController.value.isRecordingVideo) return;
+
+    await _cameraController.startVideoRecording();
+
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) return;
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
+
+    final video = await _cameraController.stopVideoRecording();
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(video: video),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _buttonAnimationController.dispose();
+    _cameraController.dispose();
+    _progressAnimationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -96,7 +155,7 @@ class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
             : Stack(
                 alignment: Alignment.center,
                 children: [
-                  CameraPreview(_cameraController),
+                  CameraPreview(_cameraController), //13분
                   Positioned(
                     top: Sizes.size28,
                     left: Sizes.size10,
@@ -147,6 +206,38 @@ class _VideoRecordingScrennState extends State<VideoRecordingScrenn> {
                           ),
                         ),
                       ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: Sizes.size40,
+                    child: GestureDetector(
+                      onTapDown: _startRecording,
+                      onTapUp: (details) => _stopRecording(),
+                      child: ScaleTransition(
+                        scale: _buttonAnimation,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                              width: Sizes.size80 + Sizes.size14,
+                              height: Sizes.size80 + Sizes.size14,
+                              child: CircularProgressIndicator(
+                                color: Colors.red.shade400,
+                                strokeWidth: Sizes.size6,
+                                value: _progressAnimationController.value,
+                              ),
+                            ),
+                            Container(
+                              width: Sizes.size80,
+                              height: Sizes.size80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.red.shade400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ],
